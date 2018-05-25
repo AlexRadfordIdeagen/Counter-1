e11e3a6368d13d4aad191584259b69e9bfef008f
DESKTOP-G1NK3QP\Alex <alexradforddesigns@gmail.com>
2018-05-25 10:15:22 +0100

    Add apple field.

--- ThingsToCount/Apple.cs
+++ ThingsToCount/Apple.cs
@@ -1,7 +1,12 @@
 ﻿namespace Counter.ThingsToCount
 {
-  public class Apple : ICountable
-  {
-    public int Count => 1;
-  }
+    public class Apple : ICountable
+    {
+        public Apple(string colour)
+        {
+            Colour = colour;
+        }
+        public string Colour { get; }
+        public int Count => 1;
+    }
 }
